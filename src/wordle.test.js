3269bcbd5a75fb079fde66f8613242e160fcfef2
Mark04 <marco.escobar@ucb.edu.bo>
2022-05-15 18:58:35 -0400

    refactorizarPruebaLetraAcertada

--- src/wordle.test.js
+++ src/wordle.test.js
@@ -14,40 +14,17 @@ describe("UCB-Wordle  -> Ganar", () => {
 
 });
 
-describe("UCB-Wordle  -> Pista palabra primera posicion", () => {
-  it("En caso de que la palabra sea 'marte' y el intento 'moler' deberia devoler 'm'", () => {
-    const wordle = new Wordle("marte");
-    expect(wordle.adivinar("moler")).toEqual("m");
-  });
-  
-  it("En caso de que la palabra sea 'palta' y el intento 'peras' deberia devoler 'p'", () => {
-    const wordle = new Wordle("palta");
-    expect(wordle.adivinar("peras")).toEqual("p");
-  });
-});
-
-
-describe("UCB-Wordle  -> Pista palabra dos posiciones", () => {
-  it("En caso de que la palabra sea 'molde' y el intento 'mosca' deberia devoler 'mo'", () => {
-    const wordle = new Wordle("molde");
-    expect(wordle.adivinar("mosca")).toEqual("mo");
-  });
-  
-  it("En caso de que la palabra sea 'carta' y el intento 'camas' deberia devoler 'mo'", () => {
-    const wordle = new Wordle("carta");
-    expect(wordle.adivinar("camas")).toEqual("ca");
-  });
-
-});
-
-
 describe("UCB-Wordle  -> Pista palabra en posicion correcta", () => {
   it("En caso de que la palabra sea 'molde' y el intento 'mosca' deberia devoler 'mo'", () => {
     const wordle = new Wordle("torta");
     expect(wordle.adivinar("morfa")).toEqual("ora");
   });  
-  it("En caso de que la palabra sea 'carta' y el intento 'choza' deberia devoler 'mo'", () => {
-    const wordle = new Wordle("carta");
-    expect(wordle.adivinar("choza")).toEqual("ca");
-  });  
+  it("En caso de que la palabra sea 'molde' y el intento 'mosca' deberia devoler 'mo'", () => {
+    const wordle = new Wordle("molde");
+    expect(wordle.adivinar("mosca")).toEqual("mo");
+  });
+  it("En caso de que la palabra sea 'palta' y el intento 'peras' deberia devoler 'p'", () => {
+    const wordle = new Wordle("palta");
+    expect(wordle.adivinar("peras")).toEqual("p");
+  });
 });
